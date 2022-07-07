9b759c38ce2949f5e76a033a407d3e0016ac6e9b
Luis-Felipe-Dev <luis.fhb.2016@gmail.com>
2022-07-06 22:10:14 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Tienda de comidas Huaranca
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
