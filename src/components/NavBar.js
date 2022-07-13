e9a348b15ba2e0923db17def164ea900d887376b
unknown <luis.fhb.2061@gmail.com>
2022-07-13 17:03:19 -0500

    change

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,22 +3,22 @@ import './NavBar.css';
 
 function NavBar(props) {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
             <div className="container-fluid">
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                <a className="navbar-brand" href="#"><img src="https://us.123rf.com/450wm/andyadi/andyadi1812/andyadi181200053/114214111-plantilla-de-dise%C3%B1o-de-logotipo-de-icono-de-vector-de-restaurante-de-comida-de-sombrero-de-chef.jpg?ver=6" alt="" /></a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                    <a className="nav-link" href="#"><img src="https://us.123rf.com/450wm/andyadi/andyadi1812/andyadi181200053/114214111-plantilla-de-dise%C3%B1o-de-logotipo-de-icono-de-vector-de-restaurante-de-comida-de-sombrero-de-chef.jpg?ver=6" alt="" /></a>
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <div className="collapse navbar-collapse" id="navbarScroll">
+                    <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
                         <li className="nav-item">
-                            <a className="nav-link" href="#nosotros">Nosotros</a>
+                            <a className="nav-link active" aria-current="page" href="#">Nosotros</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#platos">Platos</a>
+                            <a className="nav-link" href="#">Platos</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#eventos">Eventos</a>
+                            <a className="nav-link" href="#">Eventos</a>
                         </li>
                     </ul>
                     <ul className="navbar-nav">
